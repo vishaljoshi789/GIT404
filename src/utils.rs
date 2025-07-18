68c785bbe083ee1773db10dac7832197bb93a4bf
Vishal Joshi <vishalwebdev0987@gmail.com>
2025-07-18 22:30:12 +0530

    removed git internal commands and recreated read_blob function

--- src/utils.rs
+++ src/utils.rs
@@ -1,5 +1,7 @@
-use std::fs;
-use std::process::{Command};
+use std::{fs};
+// use std::process::{Command};
+use flate2::read::ZlibDecoder;
+use std::io::Read;
 
 pub fn init(){
     fs::create_dir(".git404/").expect("Error initializing your project in MY-GIT!!!");
@@ -9,6 +11,21 @@ pub fn init(){
 }
 
 pub fn read_blob(blob: String){
-    let output = Command::new("git").arg("cat-file").arg("-p").arg(blob).output().expect("Error reading the blob file");
-    println!("{:?}", output)
+
+    
+    // let output = Command::new("git").arg("cat-file").arg("-p").arg(blob).output().expect("Error reading the blob file");
+    // println!("{:?}", output)
+
+
+    let path:String = format!(".git/objects/{}/{}", &blob[..2], &blob[2..]);
+    let contents = fs::read(path)
+        .expect("Invalid");
+    let mut output = ZlibDecoder::new(contents.as_slice());
+    let mut s = String::new();
+    let _ = output.read_to_string(& mut s);
+
+    let (_, result_part) = s.split_at(8);
+    println!("{}", result_part);
 }
+
+// pub fn write_blob()
